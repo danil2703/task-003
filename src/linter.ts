8b30813a3e29db12a0b9bc1b4b03b62a46bd9e0f
danil2703 <danil2703@bk.ru>
2020-01-17 03:21:40 +0300

    first commit

---
+++ src/linter.ts
@@ -0,0 +1,55 @@
+import * as jsonToAst from "json-to-ast";
+
+export type JsonAST = jsonToAst.AstJsonEntity | undefined;
+
+export interface LinterProblem<TKey> {
+    key: TKey;
+    loc: jsonToAst.AstLocation;
+}
+
+export function makeLint<TProblemKey>(
+    json: string, 
+    validateProperty: (property: jsonToAst.AstProperty) => LinterProblem<TProblemKey>[],
+    validateObject: (property: jsonToAst.AstObject) => LinterProblem<TProblemKey>[]
+): LinterProblem<TProblemKey>[] {
+    function walk(
+        node: jsonToAst.AstJsonEntity, 
+        cbProp: (property: jsonToAst.AstProperty) => LinterProblem<TProblemKey>[],
+        cbObj: (property: jsonToAst.AstObject) => LinterProblem<TProblemKey>[],
+        errors: LinterProblem<TProblemKey>[]
+    ) {
+        console.log(json);
+        switch (node.type) {
+            case 'Array':
+                node.children.forEach((item: jsonToAst.AstJsonEntity) => {
+                    errors = walk(item, cbProp, cbObj, errors);
+                });
+                break;
+            case 'Object':
+                if(cbObj(node).length) {
+                    errors.push(...cbObj(node));
+                }
+                node.children.forEach((property: jsonToAst.AstProperty) => {
+                if(cbProp(property).length) {
+                    cbProp(property);
+                }
+                    errors = walk(property.value, cbProp, cbObj, errors);
+                });
+                break;
+        }
+        return errors;
+    }
+
+    function parseJson(json: string):JsonAST  {return jsonToAst(json); }
+
+    const errors: LinterProblem<TProblemKey>[] = [];
+    const ast: JsonAST = parseJson(json);
+    if (ast) {
+            walk(ast, 
+            (property: jsonToAst.AstProperty) => validateProperty(property), 
+            (obj: jsonToAst.AstObject) => validateObject(obj),
+            errors);
+    }
+    console.log(errors);
+    return errors;
+}
---
+++ src/server.ts
@@ -0,0 +1,144 @@
+import {
+    createConnection,
+    ProposedFeatures,
+    TextDocuments,
+    InitializeParams,
+    TextDocument,
+    Diagnostic,
+    DiagnosticSeverity,
+    DidChangeConfigurationParams
+} from 'vscode-languageserver';
+
+import { basename } from 'path';
+
+import * as jsonToAst from "json-to-ast";
+
+import { ExampleConfiguration, Severity, RuleKeys } from './configuration';
+import { makeLint, LinterProblem } from './linter';
+
+let conn = createConnection(ProposedFeatures.all);
+let docs = new TextDocuments();
+let conf: ExampleConfiguration | undefined = undefined;
+
+let hasConfigurationCapability: boolean = false;
+let hasWorkspaceFolderCapability: boolean = false;
+let hasDiagnosticRelatedInformationCapability: boolean = false;
+
+conn.onInitialize((params: InitializeParams) => {
+    return {
+		capabilities: {
+			textDocumentSync: docs.syncKind,
+			completionProvider: {
+				resolveProvider: true
+			}
+		}
+	};
+});
+
+
+function GetSeverity(key: RuleKeys): DiagnosticSeverity | undefined {
+    if (!conf || !conf.severity) {
+        return undefined;
+    }
+
+    const severity: Severity = conf.severity[key];
+    switch (severity) {
+        case Severity.Error:
+            return DiagnosticSeverity.Information;
+        case Severity.Warning:
+            return DiagnosticSeverity.Warning;
+        case Severity.Information:
+            return DiagnosticSeverity.Information;
+        case Severity.Hint:
+            return DiagnosticSeverity.Hint;
+        default:
+            return undefined;
+    }
+}
+
+function GetMessage(key: RuleKeys): string {
+    if (key === RuleKeys.BlockNameIsRequired) {
+        return 'Field named \'block\' is required!';
+    }
+
+    if (key === RuleKeys.UppercaseNamesIsForbidden) {
+        return 'Uppercase properties are forbidden!';
+    }
+
+    return `Unknown problem type '${key}'`;
+}
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+    const source = textDocument.uri;
+    const json = textDocument.getText();
+    const validateObject = (
+        obj: jsonToAst.AstObject
+    ): LinterProblem<RuleKeys>[] =>
+        obj.children.some(p => p.key.value === 'block')
+            ? []
+            : [{ key: RuleKeys.BlockNameIsRequired, loc: obj.loc }];
+
+    const validateProperty = (
+        property: jsonToAst.AstProperty
+    ): LinterProblem<RuleKeys>[] => {
+		return /^[A-Z]+$/.test(property.key.value)
+            ? [
+                  {
+                      key: RuleKeys.UppercaseNamesIsForbidden,
+                      loc: property.loc
+                  }
+              ]
+			: [];
+	};
+    const diagnostics: Diagnostic[] = makeLint(
+        json,
+        validateProperty,
+		validateObject,
+    ).reduce(
+        (
+            list: Diagnostic[],
+            problem: LinterProblem<RuleKeys>
+        ): Diagnostic[] => {
+            const severity = GetSeverity(problem.key);
+            if (severity) {
+                const message = GetMessage(problem.key);
+
+                let diagnostic: Diagnostic = {
+                    range: {
+                        start: textDocument.positionAt(
+                            problem.loc.start.offset
+                        ),
+                        end: textDocument.positionAt(problem.loc.end.offset)
+                    },
+                    severity,
+                    message,
+                    source
+                };
+                list.push(diagnostic);
+			}
+            return list;
+        },
+        []
+    );
+    if (diagnostics.length) {
+        conn.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+    }
+}
+
+async function validateAll() {
+    for (const document of docs.all()) {
+        await validateTextDocument(document);
+    }
+}
+
+docs.onDidChangeContent(change => {
+    validateTextDocument(change.document);
+});
+
+conn.onDidChangeConfiguration(({ settings }: DidChangeConfigurationParams) => {
+    conf = settings.example;
+    validateAll();
+});
+
+docs.listen(conn);
+conn.listen();
